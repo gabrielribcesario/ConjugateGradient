272491f0aef424f61e56d7408535dc5ec745d749
gabrielribcesario <gabrielc.ribeiro@hotmail.com>
2025-06-15 03:01:49 -0300

    Add Hilbert matrix example

--- src/ConjugateGradient.c
+++ src/ConjugateGradient.c
@@ -9,11 +9,12 @@ int ConjugateGradient(const int n, const int maxiter, const double tol,
     int k;
     // x is the null vector
     for (k = 0; k != n; ++k) { *(x + k) = 0.; } 
+    k = 0;
     // k-th residual vector
     double *r_k = malloc(n * sizeof(double)); 
     if (!r_k) { 
         fprintf(stderr, "r_k malloc failure\n");
-        return EXIT_FAILURE; 
+        return -1; 
     }
     // x_0 = 0 ==> r_0 = b;
     memcpy(r_k, b, n * sizeof(double)); 
@@ -28,20 +29,19 @@ int ConjugateGradient(const int n, const int maxiter, const double tol,
         if (!p_k) { 
             fprintf(stderr, "p_k malloc failure\n");
             free(r_k);
-            return EXIT_FAILURE; 
+            return -11; 
         }
-        // p_0 = r_0
-        memcpy(p_k, r_k, n * sizeof(double)); 
         // A * p_k
         double *Ap_k = calloc(n, sizeof(double)); 
         if (!Ap_k) { 
             fprintf(stderr, "Ap_k malloc failure\n");
             free(r_k);
             free(p_k);
-            return EXIT_FAILURE; 
+            return -111; 
         }
+        // p_0 = r_0
+        memcpy(p_k, r_k, n * sizeof(double)); 
         // Iterates the steps
-        printf("Running a maximum of %d iterations of the algorithm\n", maxiter);
         for (k = 0; k != maxiter; ++k) {
             // Ap_k = A * p_k
             mvmul(n, n, 1., A, p_k, 0., Ap_k);
@@ -54,11 +54,10 @@ int ConjugateGradient(const int n, const int maxiter, const double tol,
             rkp1norm = norm(n, r_k);
             // Returns the solution if ||r_(k+1)|| < tol
             if (rkp1norm < tol) {
-                printf("Converged with %g error in %d iterations\n", rkp1norm, k + 1);
                 free(r_k);
                 free(p_k);
                 free(Ap_k);
-                return EXIT_SUCCESS;
+                return k + 1;
             }
             // beta_k = ||r_(k+1)||^2 / ||r_k||^2
             double beta_k = (rkp1norm * rkp1norm) / (rknorm * rknorm);
@@ -68,12 +67,7 @@ int ConjugateGradient(const int n, const int maxiter, const double tol,
         }
         free(p_k);
         free(Ap_k);
-        printf("Could not achieve convergence with error %g\n", rknorm);
-        return EXIT_SUCCESS;
     }
-    else {
-        free(r_k);
-        printf("Returning the trivial solution with error %g\n", rknorm);
-        return EXIT_SUCCESS;
-    }
+    free(r_k);
+    return k;
 }
--- src/hilbert.c
+++ src/hilbert.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include "linalg.h"
 #include "ConjugateGradient.h"
+#include "PreconditionedCG.h"
 
 // Generates a m x n Hilbert matrix
 void HilbertMatrix(int m, int n, double *A) {
@@ -15,7 +16,7 @@ void HilbertMatrix(int m, int n, double *A) {
 
 int main(void) {
     const int n = 10000;
-    double tol = 1.E-8;
+    double tol = 1.E-10;
     int maxiter = n;
 
     double *A = malloc(n * n * sizeof(double));
@@ -31,23 +32,37 @@ int main(void) {
     }
     double *x = calloc(n, sizeof(double));
     if (!x) { 
-        fprintf(stderr, "x malloc failure\n");
+        fprintf(stderr, "x calloc failure\n");
         free(A);
         free(b);
         return EXIT_FAILURE; 
     }
+    double *sol = calloc(n, sizeof(double));
+    if (!sol) { 
+        fprintf(stderr, "sol calloc failure\n");
+        free(A);
+        free(b);
+        free(x);
+        return EXIT_FAILURE; 
+    }
 
     HilbertMatrix(n, n, A);
-    for (int i = 0; i < n; ++i) { b[i] = reduce_sum(n, &A[i * n]); }
+    // x[i] = 1
+    for (int i = 0; i < n; ++i) { b[i] = reduce_sum(n, A + i * n); }
 
     struct timespec tic, toc;
-    double elapsed;
 
+    // Time it
     clock_gettime(CLOCK_MONOTONIC, &tic);
-    ConjugateGradient(n, maxiter, tol, A, b, x);
+    int iter = ConjugateGradient(n, maxiter, tol, A, b, x);
     clock_gettime(CLOCK_MONOTONIC, &toc);
-    elapsed = (toc.tv_sec - tic.tv_sec) + (toc.tv_nsec - tic.tv_nsec) * 1.e-9;
-    printf("Ran in %.6f[s]\n", elapsed);
+    double elapsed = (toc.tv_sec - tic.tv_sec) + (toc.tv_nsec - tic.tv_nsec) * 1.e-9;
+    printf("Ran %d iterations in %.6f[s]\n", iter, elapsed);
+    // ||Ax - b||
+    mvmul(n, n, 1., A, x, 0., sol);
+    vvadd(n, 1., b, -1., sol);
+    double err = norm(n, sol);
+    printf("Returned with error %#g\n", err);
 
     free(A);
     free(b);
